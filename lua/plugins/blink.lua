5518a9618f858189ded8d8c6bd5abcc50c7ab795
ClaudioMilGitHub <c.milano96@hotmail.com>
2026-04-10 23:52:46 +0200

    config conform and blink working properly. todo > lazy loading for both plugins

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,15 +1,16 @@
-require('blink.cmp').setup({
-  keymap = { 
-      preset = 'default',
-      ['<C-space>'] = { 'show', 'show_documentation', 'hide' },
-    },
+require("blink.cmp").setup({
+	fuzzy = { implementation = "prefer_rust" },
+	keymap = {
+		preset = "default",
+		["<C-space>"] = { "show", "show_documentation", "hide" },
+	},
 
-    appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = 'mono'
-    },
+	appearance = {
+		use_nvim_cmp_as_default = true,
+		nerd_font_variant = "mono",
+	},
 
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
+	sources = {
+		default = { "lsp", "path", "snippets", "buffer" },
+	},
 })
